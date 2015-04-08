0809821930bffc0dda98fe0af6f3eba3a8d13d04
nirasan <niranirao@gmail.com>
2015-04-08 09:41:35 +0900

    create top controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root :to => 'top#index'
+  get 'top/index'
+
   get 'diagram/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
