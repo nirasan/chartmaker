a63800cbe0d4cdbb4523342be9d5e6c4225b8e1b
nirasan <niranirao@gmail.com>
2015-04-11 00:35:27 +0900

    add diagram.get_dot method

--- app/models/diagram.rb
+++ app/models/diagram.rb
@@ -1,6 +1,22 @@
+require 'gviz'
+
 class Diagram < ActiveRecord::Base
 
   belongs_to :user
   has_many :nodes
   has_many :lines
+
+  def get_dot
+    diagram = self
+    Graph do
+      diagram.nodes.each do |node|
+        id = "n#{node.id}".to_sym
+        node id, {label:node.description}
+      end
+      diagram.lines.each do |line|
+        id = "n#{line.node_id}_n#{line.next_node_id}".to_sym
+        edge id, {label:line.description}
+      end
+    end
+  end
 end
--- test/models/diagram_test.rb
+++ test/models/diagram_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class DiagramTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "get_dot" do
+    diagram = diagrams(:diagram1)
+    dot = diagram.get_dot
+    assert dot.is_a? Gviz
+    assert_equal dot.nodeset.count, 3
+    assert_equal dot.edgeset.count, 2
+    assert_not_empty dot.to_s
+  end
 end
