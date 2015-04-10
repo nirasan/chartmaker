473f92f4ee78de77a2736dec079ab2a6c4c252ca
nirasan <niranirao@gmail.com>
2015-04-10 22:37:02 +0900

    gviz test

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'erb2haml'
 
 gem 'devise'
 
+gem 'gviz'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
---
+++ lib/sample.rb
@@ -0,0 +1,35 @@
+class Sample
+  def self.hello
+    puts "hello world"
+  end
+
+  def self.gviz
+    require "gviz"
+    require "open3"
+
+    g = Graph do
+      node :start, {label: "はじめ"}
+      node :n1, {label: "のーど　いち"}
+      node :n2, {label: "のーど　に"}
+      node :n3, {label: "のーど　さん"}
+      node :end, {label: "おわり"}
+
+      edge :start_n1, {label: "のーど　いち　へ"}
+      edge :n1_n2
+      edge :n1_n3
+      edge :n2_end
+      edge :n3_end
+
+      #save(:sample1, :png)
+    end
+
+    puts g
+
+    Open3.popen3('dot -T png') do |stdin, stdout, stderr, thread|
+      stdin.puts g
+      stdin.close
+      puts stdout.read
+    end
+
+  end
+end
